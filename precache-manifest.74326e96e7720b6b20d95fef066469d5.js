e84b0cc7d3559ae8f1bbe7b7b2b6e0f4af90d66e
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2020-02-09 17:21:51 -0300

    Updates

---
+++ precache-manifest.74326e96e7720b6b20d95fef066469d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154588ebd45ba382fad865c36da3bb15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "780bf83d5746334e183d",
+    "url": "/static/js/2.f5e67cec.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f5e67cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc2bc43104aa388937f",
+    "url": "/static/js/main.b5591385.chunk.js"
+  },
+  {
+    "revision": "1087f3ec86ac14e65090",
+    "url": "/static/js/runtime-main.a9e77d1f.js"
+  }
+]);
---
+++ static/js/main.b5591385.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmatheuspiment.github.io"]=this["webpackJsonpmatheuspiment.github.io"]||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),i=n(2);var r=function(){return u.a.createElement("main",null,u.a.createElement("h1",null,"WIP"))};n.n(i).a.render(u.a.createElement(r,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.b5591385.chunk.js.map
